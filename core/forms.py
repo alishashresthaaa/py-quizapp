d6dfda7a9de64631bcc48c4a58840bf2fca489c4
alishashresthaaa <alishashresthaaa0@gmail.com>
2024-08-15 19:50:11 -0400

    template

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,8 @@
 # core/forms.py
+import re
 
 from django import forms
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(
@@ -11,6 +13,7 @@ class LoginForm(forms.Form):
         max_length=100,
         widget=forms.PasswordInput(attrs={'placeholder': 'Password'})
     )
+
 
 class RegisterForm(forms.Form):
     firstname = forms.CharField(
@@ -36,3 +39,28 @@ class RegisterForm(forms.Form):
         max_length=100,
         widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'})
     )
+
+    def clean_firstname(self):
+        firstname = self.cleaned_data.get('firstname')
+        if not firstname.isalpha():
+            raise forms.ValidationError("First name must contain only letters.")
+        return firstname
+
+    def clean_lastname(self):
+        lastname = self.cleaned_data.get('lastname')
+        if not lastname.isalpha():
+            raise forms.ValidationError("Last name must contain only letters.")
+        return lastname
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if not re.search(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$', password):
+            raise forms.ValidationError("Password must be combination of letters and numbers.")
+        return password
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+        return confirm_password
