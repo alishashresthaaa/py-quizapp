8729622b107bf7155692337f0ccd7d110d579cf9
Anchal Gurung <anchalghale@gmail.com>
2024-08-15 16:55:46 -0400

    feat: add index

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Answer
+from .models import Question
+from .models import Quiz
+from .models import QuizResponse
+
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Quiz)
+admin.site.register(QuizResponse)
--- core/models.py
+++ core/models.py
@@ -14,7 +14,15 @@ class BaseModel(models.Model):
         abstract = True
 
 
+class Category(BaseModel):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Question(BaseModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
 
     def __str__(self):
